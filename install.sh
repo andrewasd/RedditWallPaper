68272352d252d9ca86d5299e3f3c54c763fdfaea
andrewasd <99andri12@gmail.com>
2021-05-27 22:48:15 +0200

    automatically install requirments

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
+#install requirements
+
+#check node.js
+if ! command -v node &> /dev/null
+then
+packagesNeeded='nodejs'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
+fi
+
+#check curl
+if ! command -v curl &> /dev/null
+then
+packagesNeeded='curl'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
+fi
+
+
 
 INSTALL_DIR=/home/`whoami`/.RedditWallPaper
 
